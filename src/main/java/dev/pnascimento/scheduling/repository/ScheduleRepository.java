f2d9c29908c84ac4108b763e5215eed9bc34036b
Priscila Nascimento <pnmelo2808@gmail.com>
2026-01-20 15:39:16 -0300

    Feat: Add ScheduleRepository for scheduling conflict checks

---
+++ src/main/java/dev/pnascimento/scheduling/repository/ScheduleRepository.java
@@ -0,0 +1,27 @@
+package dev.pnascimento.scheduling.repository;
+
+import dev.pnascimento.scheduling.entity.schedule.Scheduling;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+
+public interface ScheduleRepository extends JpaRepository<Scheduling, Long> {
+
+    @Query("""
+            SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END
+            FROM Scheduling a
+            WHERE a.userId = :userId
+              AND a.status = status.SCHEDULED
+              AND (:excludeId IS NULL OR a.id <> :excludeId)
+            """)
+
+
+    boolean existConflict(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            @Param("excludeId") Long excludeId
+    );
+}
